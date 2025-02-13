c6e43a8d036609a4bd5a68cb39d444b04db916ff
Byteme-dot <124488563+Byteme-dot@users.noreply.github.com>
2025-02-13 18:16:27 +0530

    Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -1,44 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
 using namespace std;
-void rotateMatrix(vector<vector<int>> &matrix){
-    int n=matrix.size();
-    for(int i = 0; i < n;i++){
-        for(int j=i+1;j<n;j++){
-            swap(matrix[i][j], matrix[j][i]);
+int main(){
+    int size;
+    cout<<"Enter the size of Square matrix: ";
+    cin>>size;
+    int arr[size][size];
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+        cout<<endl<<"Enter the value for ("<<i+1<<","<<j+1<<"): ";
+        cin>>arr[i][j];
         }
     }
-    for(int i=0;i<n;i++){
-        reverse(matrix[i].begin(),matrix[i].end());
-    }
-}
-
-void printMatrix(const vector<vector<int>> &matrix){
-    for(const auto &row : matrix){
-        for(int num : row){
-            cout<<num<<"\t";
+    cout<<"Original Matrix"<<endl;
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            cout<<arr[i][j]<<"  ";
         }
         cout<<endl;
     }
-}
-
-int main(){
-    int n;
-    cout<<"Enter the size of the matrix: ";
-    cin>>n;
-    vector<vector<int>>matrix(n,vector<int>(n));
-    cout<<"Enter the elements of the matrix:"<< endl;
-    for (int i=0;i<n;i++){
-        for (int j=0;j<n;j++){
-            cout<<"Enter Element("<<i+1<<","<<j+1<<"): ";
-            cin>>matrix[i][j];
+    cout<<endl<<"90 Degree rotated Matrix"<<endl;
+    for(int i=0;i<size;i++){
+        for(int j=size-1;j>=0;j--){
+            cout<<arr[j][i]<<"  ";
         }
+        cout<<endl;
     }
-    cout << "Original Matrix:" << endl;
-    printMatrix(matrix);
-    rotateMatrix(matrix);
-    cout<<"Rotated Matrix:"<<endl;
-    printMatrix(matrix);
     return 0;
 }
